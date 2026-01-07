62e864505390b22fda1cad52c664830b8af9575e
kuutyann0330 <kuutyann0330@gmail.com>
2026-01-07 10:57:52 +0900

    Delete sotuken/templates directory
    上書き用にいったん消しました

--- sotuken/templates/main.js
+++
@@ -1,8 +0,0 @@
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function(stream) {
-        var video = document.getElementById('webcam-stream');
-        video.srcObject = stream;
-    })
-    .catch(function(err) {
-        console.error("カメラへのアクセスが拒否されました: " + err);
-    });
